77ea8bc6cb3f9b8b2cee237f48789c6f51299ded
Raphael Odini <raphodn@users.noreply.github.com>
2024-11-11 11:45:09 +0100

    Add legend

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,6 +1,15 @@
 <template>
   <l-map ref="map" v-model:zoom="mapZoom" :center="mapCenter" :use-global-leaflet="false" :options="mapOptions" @ready="initMap">
     <l-tile-layer :url="tiles" layer-type="base" name="OpenStreetMap" :attribution="attribution" />
+    <l-control position="bottomright">
+      <div style="color:black;padding:10px;background-color:white;border-radius:10px;">
+        <h4 style="margin-bottom:5px">Number of prices at the store</h4>
+        <i style="width:15px;height:15px;margin-right:5px;background-color:#72b043;display:inline-block;opacity:0.8"></i> More than 50<br>
+        <i style="width:15px;height:15px;margin-right:5px;background-color:#f8cc1b;display:inline-block;opacity:0.8"></i> Between 10 and 50<br>
+        <i style="width:15px;height:15px;margin-right:5px;background-color:#f37324;display:inline-block;opacity:0.8"></i> Less than 10<br>
+        <i style="width:15px;height:15px;margin-right:5px;background-color:#e12729;display:inline-block;opacity:0.8"></i> 0 prices
+      </div>
+    </l-control>
     <l-marker v-for="location in locations" :key="location.id" :lat-lng="[location.osm_lat, location.osm_lon]">
       <l-icon :icon-url="getLocationIconUrl(location)" :icon-size="[25, 41]" :icon-anchor="[12, 41]" :popup-anchor="[1, -34]" :shadow-size="[41, 41]" />
       <l-popup>
@@ -18,7 +27,7 @@
 
 <script>
 import 'leaflet/dist/leaflet.css'
-import { LMap, LTileLayer, LMarker, LPopup, LIcon } from '@vue-leaflet/vue-leaflet'
+import { LMap, LTileLayer, LMarker, LPopup, LIcon, LControl } from '@vue-leaflet/vue-leaflet'
 
 export default {
   components: {
@@ -27,6 +36,7 @@ export default {
     LMarker,
     LPopup,
     LIcon,
+    LControl,
   },
   props: {
     locations: {
@@ -39,13 +49,14 @@ export default {
     return {
       map: null,
       mapZoom: 3,
-      mapCenter: [30, 10],
+      mapCenter: [28, 10],
       mapBounds: null,
       mapOptions: {
         preferCanvas: true,  // doesn't work, see https://github.com/vue-leaflet/vue-leaflet/issues/199
       },
       tiles: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      // icons from https://github.com/pointhi/leaflet-color-markers
     }
   },
   mounted() {
